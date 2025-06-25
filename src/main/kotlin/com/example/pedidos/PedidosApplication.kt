075e3aebc120fb7ffd33149268718db2562e651e
Leticia Takenaka <leticiatakenaka12@gmail.com>
2025-06-25 20:06:43 -0300

    Arquivos Kotlin

---
+++ src/main/kotlin/com/example/pedidos/PedidosApplication.kt
@@ -0,0 +1,11 @@
+package com.example.pedidos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PedidosApplication
+
+fun main(args: Array<String>) {
+	runApplication<PedidosApplication>(*args)
+}
