cb749330445e3d4a30e2128e2b2303ea4b1f1f1b
Leticia Takenaka <leticiatakenaka12@gmail.com>
2025-07-06 15:34:41 -0300

    add init database to bring data at first start

--- database/init.sql
+++ database/init.sql
@@ -10,7 +10,7 @@
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
+SET statement_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
@@ -24,10 +24,29 @@ SET row_security = off;
 -- Name: public; Type: SCHEMA; Schema: -; Owner: postgres
 --
 
-CREATE SCHEMA public;
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT schema_name FROM information_schema.schemata WHERE schema_name = 'public'
+    ) THEN
+        EXECUTE 'CREATE SCHEMA public';
+END IF;
+END
+$$;
 
 
-ALTER SCHEMA public OWNER TO postgres;
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM pg_namespace
+        WHERE nspname = 'public'
+    ) THEN
+        EXECUTE 'ALTER SCHEMA public OWNER TO postgres';
+END IF;
+END
+$$;
+
 
 SET default_tablespace = '';
 
@@ -473,6 +492,126 @@ REVOKE USAGE ON SCHEMA public FROM PUBLIC;
 
 
 -- Completed on 2025-07-05 19:31:09
+
+--
+-- PostgreSQL database dump complete
+--
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.9 (Debian 16.9-1.pgdg120+1)
+-- Dumped by pg_dump version 17.5
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.users (guid, name, email, cpf, created_at, updated_at) VALUES ('af9a9bbb-ea8b-4938-be73-64ebf8d84a9e', 'leticia', 'leticia@example.com', '12345678900', '2025-07-05 21:57:29.232865', '2025-07-05 21:57:29.232865');
+
+
+--
+-- Data for Name: customers; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.customers (guid, user_guid) VALUES ('b29c3d4f-77e0-4b41-9e7d-3c1a1f6de1ef', 'af9a9bbb-ea8b-4938-be73-64ebf8d84a9e');
+
+
+--
+-- Data for Name: food_types; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.food_types (guid, description) VALUES ('8926cdfe-3462-4cd9-a89e-b3652ce15afd', 'JAPANESE');
+INSERT INTO public.food_types (guid, description) VALUES ('d26416f3-d42c-471a-8608-aafcc94c0066', 'ITALIAN');
+INSERT INTO public.food_types (guid, description) VALUES ('f3c37358-1bea-496f-9aae-f433aa5c939e', 'BURGUER');
+INSERT INTO public.food_types (guid, description) VALUES ('24e6b64e-1644-42a5-9d07-729d17e4b8ca', 'DESSERT');
+
+
+--
+-- Data for Name: restaurants; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.restaurants (guid, name, cnpj, created_at, updated_at) VALUES ('2c5565c5-2f0a-49ab-abf8-294ae4a552bc', 'Sushi House', '12.345.678/0001-90', '2025-07-05 22:34:14.211259', '2025-07-05 22:34:14.211259');
+INSERT INTO public.restaurants (guid, name, cnpj, created_at, updated_at) VALUES ('33fa4d6d-d3e5-43f5-9273-cb67f8077ea3', 'Bella Italia', '98.765.432/0001-21', '2025-07-05 22:34:14.211259', '2025-07-05 22:34:14.211259');
+INSERT INTO public.restaurants (guid, name, cnpj, created_at, updated_at) VALUES ('c8d47244-d2a7-4c49-a966-ce3b3fbf43fb', 'Burger Point', '11.222.333/0001-44', '2025-07-05 22:34:14.211259', '2025-07-05 22:34:14.211259');
+INSERT INTO public.restaurants (guid, name, cnpj, created_at, updated_at) VALUES ('2178d361-42c9-4bb5-a815-607fa2b859e8', 'Sweet Tooth', '44.555.666/0001-77', '2025-07-05 22:34:14.211259', '2025-07-05 22:34:14.211259');
+
+
+--
+-- Data for Name: items; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.items (guid, name, price, stock_quantity, restaurant_guid, food_type_guid, created_at, updated_at) VALUES ('e8a70461-b9c6-483e-96fd-47417dd50258', 'Sushi Combo', 55.00, 20, '2c5565c5-2f0a-49ab-abf8-294ae4a552bc', '8926cdfe-3462-4cd9-a89e-b3652ce15afd', '2025-07-05 22:37:07.43039', '2025-07-05 22:37:07.43039');
+INSERT INTO public.items (guid, name, price, stock_quantity, restaurant_guid, food_type_guid, created_at, updated_at) VALUES ('c87cca0f-7288-4a99-afa7-94819db93210', 'Tempura', 30.00, 15, '2c5565c5-2f0a-49ab-abf8-294ae4a552bc', '8926cdfe-3462-4cd9-a89e-b3652ce15afd', '2025-07-05 22:37:07.43039', '2025-07-05 22:37:07.43039');
+INSERT INTO public.items (guid, name, price, stock_quantity, restaurant_guid, food_type_guid, created_at, updated_at) VALUES ('6b9d5343-22c3-4ad5-a1d3-1df9a5190f7a', 'Pizza Margherita', 45.00, 25, '33fa4d6d-d3e5-43f5-9273-cb67f8077ea3', 'd26416f3-d42c-471a-8608-aafcc94c0066', '2025-07-05 22:37:07.43039', '2025-07-05 22:37:07.43039');
+INSERT INTO public.items (guid, name, price, stock_quantity, restaurant_guid, food_type_guid, created_at, updated_at) VALUES ('0e29e84b-e220-4f87-a4f3-e5cd84d09ee7', 'Spaghetti Carbonara', 38.50, 18, '33fa4d6d-d3e5-43f5-9273-cb67f8077ea3', 'd26416f3-d42c-471a-8608-aafcc94c0066', '2025-07-05 22:37:07.43039', '2025-07-05 22:37:07.43039');
+INSERT INTO public.items (guid, name, price, stock_quantity, restaurant_guid, food_type_guid, created_at, updated_at) VALUES ('9e047e88-cd40-4183-b0a6-924232f361a9', 'Classic Cheeseburger', 28.00, 30, 'c8d47244-d2a7-4c49-a966-ce3b3fbf43fb', 'f3c37358-1bea-496f-9aae-f433aa5c939e', '2025-07-05 22:37:07.43039', '2025-07-05 22:37:07.43039');
+INSERT INTO public.items (guid, name, price, stock_quantity, restaurant_guid, food_type_guid, created_at, updated_at) VALUES ('dd26a16b-6f3e-4b02-9c47-b8999e1c2b22', 'Double Bacon Burger', 35.50, 15, 'c8d47244-d2a7-4c49-a966-ce3b3fbf43fb', 'f3c37358-1bea-496f-9aae-f433aa5c939e', '2025-07-05 22:37:07.43039', '2025-07-05 22:37:07.43039');
+INSERT INTO public.items (guid, name, price, stock_quantity, restaurant_guid, food_type_guid, created_at, updated_at) VALUES ('65f89887-5273-4cd9-b576-0d46ebf3b01a', 'Chocolate Cake', 20.00, 40, '2178d361-42c9-4bb5-a815-607fa2b859e8', '24e6b64e-1644-42a5-9d07-729d17e4b8ca', '2025-07-05 22:37:07.43039', '2025-07-05 22:37:07.43039');
+INSERT INTO public.items (guid, name, price, stock_quantity, restaurant_guid, food_type_guid, created_at, updated_at) VALUES ('9f9d0064-f5aa-4445-a898-955a641d8c83', 'Ice Cream Sundae', 15.00, 50, '2178d361-42c9-4bb5-a815-607fa2b859e8', '24e6b64e-1644-42a5-9d07-729d17e4b8ca', '2025-07-05 22:37:07.43039', '2025-07-05 22:37:07.43039');
+
+
+--
+-- Data for Name: payment_types; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.payment_types (guid, description) VALUES ('fc071d3d-c523-4f4b-ab28-bd907ae05a78', 'CREDIT_CARD');
+INSERT INTO public.payment_types (guid, description) VALUES ('160e72be-ff7e-4ddb-89fa-fb1b4ce5e71c', 'DEBIT_CARD');
+INSERT INTO public.payment_types (guid, description) VALUES ('020476ff-faab-4875-ba1b-aab7d457c72a', 'PIX');
+INSERT INTO public.payment_types (guid, description) VALUES ('a5d7fcfa-c12a-4d08-96d4-43cea4e2be97', 'CASH');
+
+
+--
+-- Data for Name: orders; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- Data for Name: order_items; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- Data for Name: order_status_logs; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- Data for Name: payment_status_logs; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- Data for Name: roles; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.roles (guid, description) VALUES ('53435a56-6443-4fb0-aa4c-d33036886629', 'OWNER');
+INSERT INTO public.roles (guid, description) VALUES ('b2026230-422c-41c0-bd90-28807400d020', 'MANAGER');
+INSERT INTO public.roles (guid, description) VALUES ('1670ce41-eb82-47d5-b995-ed74c8461b9a', 'WAITER');
+
+
+--
+-- Data for Name: restaurant_staff; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
 
 --
 -- PostgreSQL database dump complete
